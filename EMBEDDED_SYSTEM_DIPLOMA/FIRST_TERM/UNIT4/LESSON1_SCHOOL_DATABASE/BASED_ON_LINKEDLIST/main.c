97a961c5d3a7c23de9b92f0ff3a48d4c351598aa
Abutalib <abdulrahmanabutalib869@gmail.com>
2023-07-30 03:02:26 +0300

    school data base based on linked list

--- EMBEDDED_SYSTEM_DIPLOMA/FIRST_TERM/UNIT4/LESSON1_SCHOOL_DATABASE/BASED_ON_LINKEDLIST/main.c
+++ EMBEDDED_SYSTEM_DIPLOMA/FIRST_TERM/UNIT4/LESSON1_SCHOOL_DATABASE/BASED_ON_LINKEDLIST/main.c
@@ -1,42 +1,55 @@
 #include "linkedlist.h"
 #include<stdio.h>
+void display (Sdata*pd)
+{   printf("********************\n");
+    printf("%d\n",pd->NUM_NAME);
+    printf("%s\n",pd->ARR_NAME);
+    printf("%.2f\n",pd->VAL_NAME);
+    printf("********************\n");
+}
 int main()
 {
-    int choice,id;
-    float heigth;
-    linkedlist student;
-    Sdata dstudent;
-    linkedlist_intialize(&student);
+    int choice,id,n;
+    float height;
+    linkedlist school;
+    Sdata student,temp;
+    linkedlist_intialize(&school);
     while (1)
     {
 
-	printf("ENTER :\n\t1-ADD STUDENT\n\t2-REMOVE STUDENT\n\t3-VIEW STUDENTS\n\t4-DELETE ALL\n");
+	printf("ENTER :\n\t1-ADD STUDENT\n\t2-REMOVE STUDENT\n\t3-VIEW STUDENTS\n\t4-DELETE ALL\n\t5-GET N REVERSE\n");
+    fflush(stdin);
 	scanf("%d",&choice);
-	Sdata temp;
 	switch (choice)
 	{
 	case 1:
 	    printf("ENTER STUDENT NAME :\n");
         fflush(stdin);
-	    gets(dstudent.name);
+	    gets(student.ARR_NAME);
 	    printf("ENTER STUDENT ID :\n");
-	    scanf("%d",&dstudent.id);
-	    printf("ENTER STUDENT HEIGTH :\n");
-	    scanf("%f",&dstudent.height);
-	    push(&student,&dstudent);
+	    scanf("%d",&student.NUM_NAME);
+	    printf("ENTER STUDENT HEIGHT :\n");
+	    scanf("%f",&student.VAL_NAME);
+	    push(&school,&student);
 	    break;
     case 2:
         printf("ENTER STUDENT ID\n");
-        scanf("%d",temp);
-        if (delete_by_id (&student,id)!=done)
-            printf("wrong id");
+        scanf("%d",&id);
+        if (delete_by_id (&school,id)!=done)
+            printf("--WRONG ID--\n");
         break;
     case 3:
-        print_linkedlist(&student);
+        print_linkedlist(&school,&display);
         break;
     case 4:
-        delete_linkedlist(&student);
+        delete_linkedlist(&school);
         break;
+	case 5:
+	    scanf("%d",&n);
+	    GetNth(&school,&student,2);
+	    display(&student);
+
+
 	}
     }
 	return 0;
